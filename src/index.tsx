3720e4ed2dfeb99fe8c3ff3c3585db7f7b38267f
Lukasz <lukaszczernal@wp.pl>
2022-05-10 00:45:10 +0200

    Github pages config

--- src/index.tsx
+++ src/index.tsx
@@ -8,7 +8,7 @@ import './index.css';
 
 render(
   () => (
-    <Router>
+    <Router base="spotifun">
       <GameProvider>
         <App />
       </GameProvider>
--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import solidPlugin from 'vite-plugin-solid';
 
 export default defineConfig({
+  base: '/spotifun/',
   plugins: [solidPlugin()],
   build: {
     target: 'esnext',
