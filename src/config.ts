fd62f5820d7a18527db61a81f245ebdc22aa51d7
Lukasz <lukaszczernal@wp.pl>
2022-05-10 00:59:01 +0200

    Redirect URL update

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 export const CLIENT_ID = 'abf93a809b6c4f2fad2df581c00c329d'; // Your client id
 // export const REDIRECT_URI = 'http://localhost:3000/spotifun/game'; // Your redirect uri
-export const REDIRECT_URI = 'https://lukaszczernal.github.io/spotifun/game'; // Your redirect uri
+export const REDIRECT_URI = 'https://lukaszczernal.github.io/spotifun/#/game'; // Your redirect uri
 export const SCOPE = 'user-read-private user-read-email user-library-read';
 export const STATE_KEY = 'spotify_auth_state';
