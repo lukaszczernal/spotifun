36bc10f7b016d8bcbbc3a8ab52d8b613461c5057
Lukasz <lukaszczernal@wp.pl>
2022-05-10 19:45:38 +0200

    Vite PWA plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,46 @@
 import { defineConfig } from 'vite';
 import solidPlugin from 'vite-plugin-solid';
+import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
   base: '/spotifun/',
-  plugins: [solidPlugin()],
+  plugins: [
+    solidPlugin(),
+    VitePWA({
+      manifest: {
+        theme_color: '#061b0e',
+        background_color: '#061b0e',
+        display: 'fullscreen',
+        scope: '/',
+        start_url: 'https://lukaszczernal.github.io/spotifun/',
+        name: 'Spotifun.live',
+        description: 'Guess cover albums of your favorite songs',
+        short_name: 'Spotifun',
+        icons: [
+          {
+            src: '/spotifun/android-chrome-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: '/spotifun/android-chrome-256x256.png',
+            sizes: '256x256',
+            type: 'image/png',
+          },
+          {
+            src: '/spotifun/android-chrome-384x384.png',
+            sizes: '384x384',
+            type: 'image/png',
+          },
+          {
+            src: '/spotifun/android-chrome-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
+    }),
+  ],
   build: {
     target: 'esnext',
     polyfillDynamicImport: false,
