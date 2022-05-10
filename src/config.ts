a82fde3dbbb37141c7855271a405a87267090bf7
Lukasz <lukaszczernal@wp.pl>
2022-05-10 16:21:10 +0200

    Deployment config

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,6 @@
 export const CLIENT_ID = 'abf93a809b6c4f2fad2df581c00c329d'; // Your client id
-export const HOST = 'http://localhost:3000/spotifun';
-// export const HOST = 'https://lukaszczernal.github.io/spotifun';
+// export const HOST = 'http://localhost:3000/spotifun';
+export const HOST = 'https://lukaszczernal.github.io/spotifun';
 export const REDIRECT_URI = `${HOST}/#/game`; // Your redirect uri
 export const SCOPE = 'user-read-private user-read-email user-library-read';
 export const STATE_KEY = 'spotify_auth_state';
