6b539e936354d9c994dd0f28418ae91c1600fdcf
Lukasz <lukaszczernal@wp.pl>
2022-05-11 23:32:12 +0200

    Player autostart

--- src/components/Player/Player.tsx
+++ src/components/Player/Player.tsx
@@ -1,5 +1,6 @@
 import { Accessor, Component, createEffect, createSignal } from 'solid-js';
 import { Track } from '../../services/useTracks';
+import { PAUSE_ICON, PLAY_ICON } from './icons';
 
 import styles from './Player.module.css';
 
@@ -7,40 +8,8 @@ interface Props {
   track: Accessor<Track | undefined>;
 }
 
-const PAUSE_ICON = (
-  <svg
-    width="19"
-    height="25"
-    viewBox="0 0 19 25"
-    fill="none"
-    xmlns="http://www.w3.org/2000/svg"
-  >
-    <path
-      fill-rule="evenodd"
-      clip-rule="evenodd"
-      d="M3.5 0C1.56701 0 0 1.567 0 3.5V21.5C0 23.433 1.56701 25 3.5 25C5.43299 25 7 23.433 7 21.5V3.5C7 1.567 5.43299 0 3.5 0ZM15.5 0C13.567 0 12 1.567 12 3.5V21.5C12 23.433 13.567 25 15.5 25C17.433 25 19 23.433 19 21.5V3.5C19 1.567 17.433 0 15.5 0Z"
-      fill="#07100B"
-    />
-  </svg>
-);
-
-const PLAY_ICON = (
-  <svg
-    width="58"
-    height="58"
-    viewBox="0 0 58 58"
-    fill="none"
-    xmlns="http://www.w3.org/2000/svg"
-  >
-    <path
-      d="M43 27.7679C44.3333 28.5378 44.3333 30.4623 43 31.2321L24.25 42.0574C22.9167 42.8272 21.25 41.8649 21.25 40.3253L21.25 18.6747C21.25 17.1351 22.9167 16.1728 24.25 16.9426L43 27.7679Z"
-      fill="#072413"
-    />
-  </svg>
-);
-
 const Player: Component<Props> = ({ track }) => {
-  const [playing, setPlaying] = createSignal<boolean>(false);
+  const [playing, setPlaying] = createSignal<boolean>(true);
   let playerRef: any; // TODO typings
   const [playbackURL, setPlaybackURL] = createSignal<string>();
 
@@ -59,7 +28,7 @@ const Player: Component<Props> = ({ track }) => {
   createEffect(() => {
     setPlaybackURL(track()?.track.preview_url);
     playerRef?.load();
-    setPlaying(false);
+    playerRef?.play();
   });
 
   return (
