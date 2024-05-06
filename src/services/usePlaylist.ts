4c1ea447ef8b1d793c9cb8406387b1098fa2e121
Lukasz Czernal <lukasz.czernal@inflect.com>
2024-05-06 23:41:47 +0200

    Mock playlist path fix

--- src/services/usePlaylist.ts
+++ src/services/usePlaylist.ts
@@ -4,7 +4,7 @@ import { Track, Playlist } from './model';
 import { useAuth } from './useAuth';
 
 // const PLAYLIST_URL = 'https://api.spotify.com/v1/playlists/70N5mgNl3QBQB09zXoa72h';
-const PLAYLIST_URL = 'src/assets/mocks/playlist.json';
+const PLAYLIST_URL = 'api/mocks/playlist.json';
 
 export const fetchPlaylist = () => {
   const { getAccessToken } = useAuth();
