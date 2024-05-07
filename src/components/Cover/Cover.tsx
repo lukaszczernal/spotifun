16490f020c7ad2eaa901166ce72d2a46e507411e
Lukasz Czernal <lukasz.czernal@inflect.com>
2024-05-07 21:54:18 +0200

    Overlapping cover animation fix

--- src/components/Cover/Cover.tsx
+++ src/components/Cover/Cover.tsx
@@ -1,6 +1,8 @@
-import { Component, createEffect } from 'solid-js';
-import { Track } from '../../services/model';
-import styles from './Cover.module.css';
+import anime from "animejs";
+import { Component, onMount, onCleanup, createEffect } from "solid-js";
+import { Track } from "../../services/model";
+
+import styles from "./Cover.module.css";
 
 interface Props {
   track?: Track;
@@ -12,35 +14,61 @@ interface Props {
 }
 
 const transformMap = [
-  { bottom: '-107%', right: '-107%' },
-  { bottom: '-107%', left: '-107%' },
-  { top: '-107%', right: '-107%' },
-  { top: '-107%', left: '-107%' },
+  { translateY: "51%", translateX: "52%" },
+  { translateY: "51%", translateX: "-52%" },
+  { translateY: "-51%", translateX: "52%" },
+  { translateY: "-51%", translateX: "-52%" },
 ];
-
-const getSelectedStyle = (index: number) => ({
-  'z-index': 3,
-  ...transformMap[index],
-});
 
 const Cover: Component<Props> = (props) => {
   let coverRef: HTMLAnchorElement;
 
+  const onClickCallback = () => {
+    console.log("!! cover clicked");
+    props.onClick(props.track, props.position);
+  };
+
   createEffect(() => {
-    const hammerCover = new Hammer(coverRef, {
-      recognizers: [[Hammer.Swipe], [Hammer.Tap]],
-    });
-    hammerCover.on('swipe tap', () => {
-      props.onClick(props.track, props.position);
-    });
+    if (props.isSelected) {
+      anime({
+        targets: coverRef,
+        zIndex: {
+          value: 30,
+          duration: 0,
+        },
+        scale: 2.1,
+        ...transformMap[props.position],
+      });
+    } else {
+      anime({
+        targets: coverRef,
+        translateX: "0%",
+        translateY: "0%",
+        scale: 1,
+        zIndex: {
+          value: 0,
+          duration: 100,
+        },
+      });
+    }
+  });
+
+  onMount(() => {
+    new Hammer(coverRef, {
+      recognizers: [[Hammer.Tap]],
+    }).on("tap", onClickCallback);
+  });
+
+  onCleanup(() => {
+    console.log("TODO check if remove works if game ends");
+    Hammer.off(coverRef, "tap", onClickCallback);
   });
 
   return (
-    <div className={styles.cover__placeholder} >
+    <div className={styles.cover__placeholder}>
       <a
-        style={props.isSelected ? getSelectedStyle(props.position) : {}}
         className={styles.cover}
-        class={`cover ${props.isCorrect ? 'cover__correct': ''}`} // TODO I do not like this solution
+        class={`cover ${props.isCorrect ? "cover__correct" : ""}`} // TODO I do not like this solution
         ref={coverRef}
       >
         <img src={props.track?.album.images[0].url} onLoad={props.onLoad} />
